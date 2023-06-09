48d27084b43945b863dd29ed64f3a5a01f2d4702
Dhakchayini <a.n.swathyraj@gmail.com>
2023-06-09 11:48:26 +0530

    completed

---
+++ src/ord/Sample/Sample.java
@@ -0,0 +1,20 @@
+package ord.Sample;
+
+public class Sample {
+	
+	public static int add() {
+
+		int a= 10;
+		int b = 20;
+		
+		return  a+b;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(add());
+		
+	}
+
+}
